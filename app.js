ca52a73c09ac86be67ea7f0bb5d0bd1a61ebbb5d
Zack Bloom <zackbloom@gmail.com>
2016-02-17 15:30:09 -0500

    Restructuring and format improvements

--- app.js
+++ app.js
@@ -1,115 +1,118 @@
 (function(){
+    if (!document.documentElement.addEventListener){
+        return;
+    }
+
     var options = INSTALL_OPTIONS;
-    var gd, sd, count = 0, banner, show = false, created = false;
-    var getPage = function() {
-        $.get(window.location.href, function(d){gd = d;});
+    var isPreview = INSTALL_ID == 'preview';
+
+    var styleEl = document.createElement('style');
+    document.head.appendChild(styleEl);
+
+    var prevPageContent, errorCount = 0, banner, show = false;
+    var getPage = function(cb) {
+        var xhr = new XMLHttpRequest;
+        xhr.open('GET', window.location.href, true);
+        xhr.addEventListener('load', function(){
+            cb(false, xhr.responseText);
+        });
+        xhr.addEventListener('error', function(err){
+            cb(true, err);
+        });
+        xhr.send();
     };
+
     var createBanner = function() {
-        var b = document.createElement('div');
-        var t = document.createElement('p');
-        b.appendChild(t);
-        banner = $(b);
-        banner.attr('id', 'ud-Banner');
-        banner.css({'top':-options.banner.bh+'px'});
-        document.body.appendChild(b);
-        created = true;
+        banner = document.createElement('ud-banner');
+        var text = document.createElement('ud-banner-content');
+
+        banner.appendChild(text);
+        banner.style.top = -options.banner.bannerHeight + 'px';
+        document.body.appendChild(banner);
+
+        banner.addEventListener('click', function(){
+            document.location.reload();
+        });
     };
+
+    var check = function() {
+        getPage(function(err, body){
+            if (err){
+                errorCount++;
+                console.error("Error testing for updates", err);
+            } else {
+                errorCount = 0;
+
+                if (body !== prevPageContent){
+                    show = true;
+                    update();
+                }
+
+                prevPageContent = body;
+            }
+
+            if (errorCount < 3){
+                window.setTimeout(check, options.time * 1000);
+            }
+        });
+    }
+
     var setup = function() {
-        getPage();
-        if (gd == '' || typeof gd === 'undefined') {
-            retry();
-            return;
-        }
-        count = 0;
-        sd = gd;
-        timer();
-    };
-    var retry = function() {
-        if (count >= 3) return;
-        count++;
-        window.setTimeout(function() {
-            setup();
-        }, 5000);
-    };
-    var timer = function() {
-        window.setTimeout(function() {
-            getPage();
-            if (gd == '' || typeof gd === 'undefined') {
-                retryTimer();
-                return;
-            }
-            count = 0;
-            if (sd == '' || typeof sd === 'undefined') {
-                sd = gd;
-            }
-            if (gd != sd) {
-                show = true;
-                update();
-            }
-            else {
-                retryTimer(0);
-            }
-        }, options.time*1000);
-    };
-    var retryTimer = function(d) {
-        if (count >= 3) return;
-        count++;
-        window.setTimeout(function() {
-            timer();
-        }, (d ? d : 5000));
-    };
+        window.setTimeout(check, options.time * 1000);
+    }
+
     var update = function() {
-        if (options.reload && !options.banner.preview) {
+        if (options.reload && !isPreview) {
             location.reload();
             return;
         }
-        if (!created) createBanner();
-        if (options.banner.theme == 'Custom...') {
-            var col = options.banner.ct;
-            col = 'rgba('+parseInt(col.slice(-6, -4), 16)+','+parseInt(col.slice(-4, -2), 16)+','+parseInt(col.slice(-2), 16)+','+(options.banner.t ? 0.5 : 1);
-            banner.css({
-                'background': col,
-                'height': options.banner.bh+'px',
-                'line-height': options.banner.bh+'px'
-            });
-            banner.children().css({
-                'font-family': (options.banner.font == '' ? 'sans-serif' : options.banner.font),
-                'font-size': options.banner.ts+'px',
-                'color': options.banner.tc
-            });
+
+        if (!banner)
+            createBanner();
+
+        banner.className = 'ud-banner';
+
+        var bgColor = ''
+        var textColor = ''
+        if (options.banner.theme == 'custom') {
+            var col = options.banner.backgroundColor;
+            bgColor = 'rgba('+parseInt(col.slice(-6, -4), 16)+','+parseInt(col.slice(-4, -2), 16)+','+parseInt(col.slice(-2), 16)+','+(options.banner.transparent ? 0.8 : 1)+')';
+            textColor = options.banner.textColor;
+        } else {
+            banner.className += ' ud-' + options.banner.theme + (options.banner.transparent ? '-t' : '');
         }
-        else {
-            banner.css({
-                'background':'',
-                'height': options.banner.bh+'px',
-                'line-height': options.banner.bh+'px'
-            });
-            banner.removeClass(banner.attr('class')).addClass('ud-' + options.banner.theme + (options.banner.t ? '-t' : ''));
-            banner.children().css({
-                'font-family': (options.banner.font == '' ? 'sans-serif' : options.banner.font),
-                'font-size': options.banner.ts+'px',
-                'color': ''
-            });
-        }
-        banner.children().html((options.banner.text == '' ? 'This page has been updated. Reload to see new version.' : options.banner.text));
-        $(banner).animate({'top': ((show || options.banner.preview) ? '0' : -options.banner.bh) + 'px'});
+        console.log(bgColor, textColor);
+
+        styleEl.innerHTML = 'ud-banner {' +
+            'background-color: ' + bgColor + ';' +
+            'height: ' + options.banner.height + 'px;' +
+        '}' +
+        'ud-banner-content {' +
+            'font-family: ' + (options.banner.font == '' ? 'sans-serif' : options.banner.font) + ';' +
+            'font-size: ' + options.banner.textSize + 'px;' +
+            'color: ' + textColor + ';' +
+            'line-height: ' + options.banner.height + 'px;' +
+        '}';
+
+        banner.querySelector('ud-banner-content').innerHTML = options.banner.text || 'This page has been updated. Reload to see new version.';
+        if ((show || isPreview) && !options.reload)
+            banner.style.top = 0;
+        else
+            banner.style.top = -options.banner.height + 'px';
     };
+
     var setOptions = function(s) {
         options = s;
         update();
     };
+
     window.UpdateDetector = {
         setOptions: setOptions
     };
-    if (window.jQuery) {
-        if (options.banner.preview) {
-            update();
-        }
-        else {
-            setup();
-        }
+
+    if (isPreview) {
+        update();
     }
-    else {
-        console.log('jQuery not detected, disabling UpdateDetector.');
-    }
+
+    setup();
 })()
